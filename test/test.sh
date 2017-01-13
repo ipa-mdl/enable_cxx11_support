a4e36a10f5121d18d039dea1454325cb73ebb808
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-01-13 18:24:23 +0100

    codacy fixes

--- test/test.sh
+++ test/test.sh
@@ -2,8 +2,8 @@
 
 rm -rf /tmp/catkin_ws/src
 mkdir -p /tmp/catkin_ws/src
-ln -s `pwd` /tmp/catkin_ws/src/enable_cxx11_support
-ln -s `pwd`/test/src /tmp/catkin_ws/src/others
+ln -s $(pwd) /tmp/catkin_ws/src/enable_cxx11_support
+ln -s $(pwd)/test/src /tmp/catkin_ws/src/others
 
 cd /tmp/catkin_ws/src
 git clone https://github.com/ros/catkin.git --depth 1
